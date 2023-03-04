d2dbb004c4a9c2f01ee73ea20a57225b1b883317
Colin Le <colintranle123@gmail.com>
2023-03-04 15:32:45 -0500

    Added routes and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <p>
-      hello
-    </p>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
